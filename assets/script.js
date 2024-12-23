0569162067bf4c83c9c598ecea779c52355a1f4e
Anubhav Kumar Gupta <122034618+AnubhavKumarGupta@users.noreply.github.com>
2024-12-23 11:39:36 +0530

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,32 @@
-const genBtn = document.getElementById("generatebtn"); // Get the generate button
-const copyBtn = document.getElementById("copyBtn"); // Get the copy button
-const textField = document.getElementById("textField"); // Get the text field
-const copyMessage = document.getElementById("copyMessage"); // Get the copy message
+const genBtn = document.getElementById("generatebtn"); // yha generate button create kr rhe hum
+const copyBtn = document.getElementById("copyBtn");  // yha copy button create kr rhe hum
+const textField = document.getElementById("textField"); // yha text field create kr rhe hum
+const copyMessage = document.getElementById("copyMessage"); // yha copy message create kr rhe hum
 
-copyBtn.addEventListener("click", copyPassword); // Add event listener to the copy button
-genBtn.addEventListener("click", generatePassword); // Add event listener to the generate button
+copyBtn.addEventListener("click", copyPassword); // yha add event listener kr rhe hum copy button pr 
+genBtn.addEventListener("click", generatePassword); // yha add event listener kr rhe hum generate button pr
 
 function generatePassword() {
-  // Function to generate a random password
+  // ye function password generate krne k liye h
   const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789=*~`!@#$%^&*()_-+={[}]|:;<,>.?/"; // Characters to be used in the password
-  const passLength = 16; // Length of the password
-  let password = ""; // Variable to store the password
-  for (let i = 0; i < passLength; i++) { // Loop to generate the password
-    let index = Math.random() * characters.length; // Generate a random index
-    password += characters.charAt(index); // Add the character at the generated index to the password
-  } // End of loop
-  textField.value = password; // Set the password to the text field
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789=*~`!@#$%^&*()_-+={[}]|:;<,>.?/"; // ye sub characters h jo password me use honge
+  const passLength = 16; // ye password ki length h
+  let password = ""; // ye password naam ka variable h jisme password store hoga
+  for (let i = 0; i < passLength; i++) { // ye loop h jo password generate krne k liye h
+    let index = Math.random() * characters.length; // ye ek random index generate kr rha h
+    password += characters.charAt(index); // ye password me characters add kr rha h
+  } 
+  textField.value = password; // ye password ko text field me show kr rha h
 }
 
-function copyPassword() { // Function to copy the password to the clipboard
-  if (textField.value != "") { // Check if the text field is not empty
+function copyPassword() { // ye function password ko copy krne k liye h
+  if (textField.value != "") { // check kr rhe ki text field empty hai ya nhi
     navigator.clipboard.writeText(textField.value); // Copy the password to the clipboard
     copyMessage.style.display = "block"; // Display the copied message
     setTimeout(() => { // Set a timeout
       copyMessage.style.display = "none"; // Hide the copied message after 0.5 seconds
-    }, 500); // End of timeout
-  } else { // If the text field is empty display an alert message to the user to generate a password first before copying it to the clipboard 
-    window.alert("Click the generate button"); // Alert message 
-  } // End of if statement
-} // End of function
+    }, 500); // 0.5 seconds 
+  } else { // ydi text field empty hai to ek alert message show krega ki password generate kro phle 
+    window.alert("Click the generate button"); // ye alert message show kr rha h
+  } 
+} 
